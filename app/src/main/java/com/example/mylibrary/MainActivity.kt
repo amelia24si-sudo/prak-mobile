217c39683f354a8d0948bfca2d5d8dc71130a75b
NabilaSurya <surya24si@mahasiswa.pcr.ac.id>
2026-04-08 08:26:12 +0700

    Pertemuan 4 Kirim data Intent dan Lifecycle

--- app/src/main/java/com/example/mylibrary/MainActivity.kt
+++ app/src/main/java/com/example/mylibrary/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.mylibrary
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mylibrary.databinding.ActivityMainBinding
+import com.example.mylibrary.databinding.ActivityThirdBinding
+import com.example.mylibrary.pertemuan_3.ThirdResultActivity
+import com.example.mylibrary.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnToFourrth.setOnClickListener { //pakai binding untuk lebih mempermudah pemanggilan id
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            Toast.makeText(
+                this,
+                "Anda telah memasuki halaman Fourth Activity",
+                Toast.LENGTH_SHORT
+            ).show()
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
+            startActivity(intent)
+            finish()
         }
     }
 }
---
+++ app/src/main/java/com/example/mylibrary/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,58 @@
+package com.example.mylibrary.pertemuan_4
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mylibrary.MainActivity
+import com.example.mylibrary.R
+import com.example.mylibrary.databinding.ActivityFourthBinding
+import com.example.mylibrary.databinding.ActivityMainBinding
+import com.example.mylibrary.databinding.ActivityThirdBinding
+import kotlin.math.log
+
+class FourthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("=== onCreate ===", "FourthActivity dibuat pertama kali")
+        enableEdgeToEdge()
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnToMain.setOnClickListener { //pakai binding untuk lebih mempermudah pemanggilan id
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            Toast.makeText(
+                this,
+                "Anda telah memasuki halaman Main Activity",
+                Toast.LENGTH_SHORT
+            ).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+        Log.e("== Data Intent ==","Nama: $name , Usia: $age, Asal: $from")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("=== onStart ===", "onStart: FourthActivity terlihat di layar")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("=== onDestroy ===", "FourthActivity dihapus dari stack")
+    }
+}
